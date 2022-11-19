ea45959f95adc7e42db79bb29ba9db0d928cabe5
Zarivate <Zarivate1@gmail.com>
2022-11-18 23:58:48 -0600

    For some reason a tailwind style is giving back an error but doesn't break program

--- components/RideOptions.js
+++ components/RideOptions.js
@@ -1,11 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "twrnc";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const RideOptions = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>RideOptions</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 left-5 p-3 rounded-full`}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
