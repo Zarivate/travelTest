096ee4ab036eb0353da2c87449d9a0d85367bb44
Zarivate <Zarivate1@gmail.com>
2022-11-19 23:58:45 -0600

    Tailwind error doesn't pop up anymore

--- components/RideOptions.js
+++ components/RideOptions.js
@@ -1,9 +1,30 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "twrnc";
 import { Icon } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
+
+const basicData = [
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multipplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-XL-456",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+];
 
 const RideOptions = () => {
   const navigation = useNavigation();
@@ -13,12 +34,13 @@ const RideOptions = () => {
       <View>
         <TouchableOpacity
           onPress={() => navigation.navigate("NavigateCard")}
-          style={tw`absolute top-3 left-5 p-3 rounded-full`}
+          style={tw`absolute top-3 z-50 left-5 p-3 rounded-full`}
         >
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
         <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
       </View>
+      <FlatList></FlatList>
     </SafeAreaView>
   );
 };
