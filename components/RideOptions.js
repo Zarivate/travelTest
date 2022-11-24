0a1a6a7db742229d46792ea138856e22868ad729
Zarivate <Zarivate1@gmail.com>
2022-11-23 19:57:06 -0600

    Added more features to ride options screen

--- components/RideOptions.js
+++ components/RideOptions.js
@@ -4,8 +4,10 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Image,
+  ScrollView,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "twrnc";
 import { Icon } from "react-native-elements";
@@ -15,7 +17,7 @@ const basicData = [
   {
     id: "Uber-X-123",
     title: "UberX",
-    multipplier: 1,
+    multiplier: 1,
     image: "https://links.papareact.com/3pn",
   },
   {
@@ -24,23 +26,65 @@ const basicData = [
     multiplier: 1.2,
     image: "https://links.papareact.com/5w8",
   },
+  {
+    id: "Uber-LUX-789",
+    title: "Uber LUX",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
 ];
 
 const RideOptions = () => {
   const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
 
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
       <View>
         <TouchableOpacity
           onPress={() => navigation.navigate("NavigateCard")}
-          style={tw`absolute top-3 z-50 left-5 p-3 rounded-full`}
+          style={tw`absolute bottom--3 z-50 left-5 p-3 rounded-full`}
         >
-          <Icon name="chevron-left" type="fontawesome" />
+          <Icon name="chevron-left" type="fontawesome" size={25} />
         </TouchableOpacity>
-        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+        <Text style={tw`text-center text-xl`}>Select a Ride</Text>
       </View>
-      <FlatList></FlatList>
+      <FlatList
+        data={basicData}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, title, multiplier, image }, item }) => (
+          <TouchableOpacity
+            onPress={() => setSelected(item)}
+            style={tw`flex-row justify-between items-center px-10 ${
+              id === selected?.id && "bg-gray-200"
+            }`}
+          >
+            <Image
+              style={{
+                width: 80,
+                height: 80,
+                resizeMode: "contain",
+              }}
+              source={{ uri: image }}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>Travel time...</Text>
+            </View>
+            <Text style={tw`text-xl`}>$Price</Text>
+          </TouchableOpacity>
+        )}
+      />
+      <View>
+        <TouchableOpacity
+          disabled={!selected}
+          style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
+        >
+          <Text style={tw`text-center text-white text-xl`}>
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
