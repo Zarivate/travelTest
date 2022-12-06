a435a5f831f7d552b813ddb290d357bffd97c9bd
Zarivate <Zarivate1@gmail.com>
2022-12-05 23:59:09 -0600

    Restarting food screen again

--- screens/EatsScreen.js
+++ screens/EatsScreen.js
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "twrnc";
@@ -14,18 +14,13 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      <View style={tw`p-5`}>
-        <Image
-          style={{
-            width: 100,
-            height: 100,
-            resizeMode: "contain",
-          }}
-          source={{
-            uri: "https://w1.pngwing.com/pngs/546/859/png-transparent-food-icon-delivery-icon-sushi-pizza-delivery-scooter-courier-food-delivery-text-thumbnail.png",
-          }}
-        />
-        <Text>Had to remove just about everything</Text>
+      <Text>Select a restaurant</Text>
+      <View>
+        <Text>
+          <View>
+            <TouchableOpacity></TouchableOpacity>
+          </View>
+        </Text>
       </View>
     </SafeAreaView>
   );
