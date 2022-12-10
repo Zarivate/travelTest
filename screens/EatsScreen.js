327ab64f550dfd943640e0305f56e6e1fa6c16ff
Zarivate <Zarivate1@gmail.com>
2022-12-09 23:59:52 -0600

    Testing

--- screens/EatsScreen.js
+++ screens/EatsScreen.js
@@ -2,13 +2,21 @@ import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "twrnc";
-import NavOptions from "../components/NavOptions";
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch, useSelector } from "react-redux";
-import { setDestination, setOrigin } from "../slices/navSlice";
 import NavFavourites from "../components/NavFavourites";
-//import Wcdonalds from "../assets/WcDonalds.jpg";
+import wcdonalds from "../assets/WcDonalds.jpg";
+import remy from "../assets/Remy.jpg";
+import kingMeat from "../assets/kingMeat";
+import angery from "../assets/Angery";
+import theRat from "../assets/theRat";
+import bigW from "../assets/epic.jpg";
+import {
+  ArrowLeftIcon,
+  ChevronRightIcon,
+  StarIcon,
+  MapPinIcon,
+  QuestionMarkCircleIcon,
+} from "react-native-heroicons/solid";
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
@@ -29,18 +37,66 @@ const HomeScreen = () => {
   ];
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      <Text>Select a restaurant</Text>
-      <View>
-        <Text>
-          <View>
-            <TouchableOpacity>
-              <View>
-                <Text>Restaurants loading...</Text>
+      <ScrollView>
+        <View className="relative">
+          <Image
+            source={{
+              uri: urlFor(imgUrl).url(),
+            }}
+            className="w-full h-56 bg-gray-300 p-4"
+          />
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full"
+          >
+            <ArrowLeftIcon size={20} color="#00CCBB" />
+          </TouchableOpacity>
+        </View>
+        <View className="bg-white">
+          <View className="px-4 pt-4">
+            <Text className="text-3xl font-bold">{title}</Text>
+            <View className="flex-row space-x-2 my-1">
+              <View className="flex-row items-center space-x-1">
+                <StarIcon color="green" opacity={0.5} size={22} />
+                <Text className="text-xs text-gray-500">
+                  <Text className="text-green-500">{rating}</Text> - {genre}
+                </Text>
               </View>
-            </TouchableOpacity>
+
+              <View className="flex-row items-center space-x-1">
+                <MapPinIcon color="gray" opacity={0.6} size={22} />
+                <Text className="text-xs text-gray-500">
+                  Nearby - {address}{" "}
+                </Text>
+              </View>
+            </View>
+            <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
           </View>
-        </Text>
-      </View>
+
+          <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+            <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
+            <Text className="pl-2 flex-1 text-md font-bold">
+              Have a food allergy?
+            </Text>
+            <ChevronRightIcon color="#00CCBB" />
+          </TouchableOpacity>
+        </View>
+
+        <View className="pb-36">
+          <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+          {/* Dishes component goes here */}
+          {dishes.map((dish) => (
+            <DishRow
+              key={dish._id}
+              id={dish._id}
+              name={dish.name}
+              description={dish.short_description}
+              price={dish.price}
+              image={dish.image}
+            />
+          ))}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
