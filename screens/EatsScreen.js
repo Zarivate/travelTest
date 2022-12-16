59dc2002dbfdf57b75a5f0749298a0ab6c548b34
Zarivate <Zarivate1@gmail.com>
2022-12-15 23:55:26 -0600

    Updated dependencies and fixed more styling errors

--- screens/EatsScreen.js
+++ screens/EatsScreen.js
@@ -35,6 +35,9 @@ const HomeScreen = () => {
       image: wcdonalds,
       screen: "EatsScreen",
       description: "No copyright, all original beef",
+      address: "9 E. Wild Rose St.Eau Claire, WI 54701",
+      rating: 4.5,
+      genre: "Fast food",
     },
     {
       id: "456",
@@ -42,13 +45,19 @@ const HomeScreen = () => {
       image: remy,
       screen: "EatsScreen",
       description: "The one from Pixar that makes the good food",
+      address: "109A South Dr. Rock Hill, SC 29730",
+      rating: 5,
+      genre: "Italian",
     },
     {
       id: "789",
-      title: "exquisite",
+      title: "All American",
       image: chicken,
       screen: "EatsScreen",
       description: "An actually normal restaurant ",
+      address: "33 Henry St. Grand Blanc, MI 48439",
+      rating: 1.5,
+      genre: "American",
     },
     {
       id: "101112",
@@ -56,6 +65,9 @@ const HomeScreen = () => {
       image: kingMeat,
       screen: "EatsScreen",
       description: "Ungodly flavor not even the Gods could believe",
+      address: "233 Third Street Montgomery, AL 36109",
+      rating: 4.7,
+      genre: "Seafood",
     },
     {
       id: "131415",
@@ -63,6 +75,9 @@ const HomeScreen = () => {
       image: bigW,
       screen: "EatsScreen",
       description: "Something special just for you",
+      address: "95 Bay Dr. Stillwater, MN 55082",
+      rating: 5,
+      genre: "Sonic",
     },
     {
       id: "161718",
@@ -70,6 +85,9 @@ const HomeScreen = () => {
       image: angery,
       screen: "EatsScreen",
       description: "Anime girl cuisine served by kind waitresses",
+      address: "8 Bayport St. Port Charlotte, FL 33952",
+      rating: 3.5,
+      genre: "Anime",
     },
     {
       id: "192021",
@@ -78,50 +96,57 @@ const HomeScreen = () => {
       screen: "EatsScreen",
       description:
         "Do not ever go here, you will only find pain and suffering and lotsa cheese.",
+      address: "7142 Beechwood Rd. Los Banos, CA 93635",
+      rating: 1.7,
+      genre: "Awful",
     },
   ];
 
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <ScrollView>
-        <View className="relative">
+        <View style={tw`relative`}>
           {sampleData.map((data) => {
             return (
               <>
                 <Image
-                  source={{
-                    uri: data.image,
-                  }}
-                  className="w-full h-56 bg-gray-300 p-4"
+                  source={data.image}
+                  style={tw`w-full h-56 bg-gray-300 p-4`}
                 />
-                <View className="bg-white">
-                  <View className="px-4 pt-4">
-                    <Text className="text-3xl font-bold">{data.title}</Text>
-                    <View className="flex-row space-x-2 my-1">
-                      <View className="flex-row items-center space-x-1">
+                <View style={tw`bg-white`}>
+                  <View style={tw`px-4 pt-4`}>
+                    <Text style={tw`text-3xl font-bold`}>{data.title}</Text>
+                    <View style={tw`flex-row space-x-2 my-1`}>
+                      <View style={tw`flex-row items-center space-x-1`}>
                         <StarIcon color="green" opacity={0.5} size={22} />
+                        <Text className="text-xs text-gray-500">
+                          <Text className="text-green-500">{data.rating}</Text>{" "}
+                          - {data.genre}
+                        </Text>
                       </View>
 
-                      <View className="flex-row items-center space-x-1">
+                      <View style={tw`flex-row items-center space-x-1`}>
                         <MapPinIcon color="gray" opacity={0.6} size={22} />
-                        <Text className="text-xs text-gray-500">
-                          Nearby - {address}{" "}
+                        <Text style={tw`text-xs text-gray-500`}>
+                          Nearby - {data.address}{" "}
                         </Text>
                       </View>
                     </View>
-                    <Text className="text-gray-500 mt-2 pb-4">
+                    <Text style={tw`text-gray-500 mt-2 pb-4`}>
                       {data.description}
                     </Text>
                   </View>
 
-                  <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+                  <TouchableOpacity
+                    style={tw`flex-row items-center space-x-2 p-4 border-y border-gray-300`}
+                  >
                     <QuestionMarkCircleIcon
                       color="gray"
                       opacity={0.6}
                       size={20}
                     />
-                    <Text className="pl-2 flex-1 text-md font-bold">
-                      Have a food allergy?
+                    <Text style={tw`pl-2 flex-1 text-md font-bold`}>
+                      Have a food allergy? Good
                     </Text>
                     <ChevronRightIcon color="#00CCBB" />
                   </TouchableOpacity>
@@ -129,19 +154,6 @@ const HomeScreen = () => {
               </>
             );
           })}
-        </View>
-        <View className="pb-36">
-          <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
-          {dishes.map((dish) => (
-            <DishRow
-              key={dish._id}
-              id={dish._id}
-              name={dish.name}
-              description={dish.short_description}
-              price={dish.price}
-              image={dish.image}
-            />
-          ))}
         </View>
       </ScrollView>
     </SafeAreaView>
