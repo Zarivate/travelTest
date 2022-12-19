d82e7ab454f1cfce0c30915a2b1e68d6db81d090
Zarivate <Zarivate1@gmail.com>
2022-12-18 23:47:58 -0600

    Reformatted eatscreen layout

--- screens/EatsScreen.js
+++ screens/EatsScreen.js
@@ -107,10 +107,10 @@ const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <ScrollView>
-        <View style={tw`relative`}>
-          {sampleData.map((data) => {
-            return (
-              <>
+        {sampleData.map((data) => {
+          return (
+            <>
+              <View style={tw`relative`}>
                 <Image
                   source={data.image}
                   style={tw`w-full h-56 bg-gray-300 p-4`}
@@ -121,49 +121,48 @@ const HomeScreen = () => {
                 >
                   <ArrowLeftIcon size={20} color="#00CCBB" />
                 </TouchableOpacity>
-
-                <View style={tw`bg-white`}>
-                  <View style={tw`px-4 pt-4`}>
-                    <Text style={tw`text-3xl font-bold`}>{data.title}</Text>
-                    <View style={tw`flex-row space-x-2 my-1`}>
-                      <View style={tw`flex-row items-center space-x-1`}>
-                        <StarIcon color="green" opacity={0.5} size={22} />
-                        <Text className="text-xs text-gray-500">
-                          <Text className="text-green-500">{data.rating}</Text>{" "}
-                          - {data.genre}
-                        </Text>
-                      </View>
-
-                      <View style={tw`flex-row items-center space-x-1`}>
-                        <MapPinIcon color="gray" opacity={0.6} size={22} />
-                        <Text style={tw`text-xs text-gray-500`}>
-                          Nearby - {data.address}{" "}
-                        </Text>
-                      </View>
+              </View>
+              <View style={tw`bg-white`}>
+                <View style={tw`px-4 pt-4`}>
+                  <Text style={tw`text-3xl font-bold`}>{data.title}</Text>
+                  <View style={tw`flex-row space-x-2 my-1`}>
+                    <View style={tw`flex-row items-center space-x-1`}>
+                      <StarIcon color="green" opacity={0.5} size={22} />
+                      <Text className="text-xs text-gray-500">
+                        <Text className="text-green-500">{data.rating}</Text> -{" "}
+                        {data.genre}
+                      </Text>
                     </View>
-                    <Text style={tw`text-gray-500 mt-2 pb-4`}>
-                      {data.description}
-                    </Text>
-                  </View>
 
-                  <TouchableOpacity
-                    style={tw`flex-row items-center space-x-2 p-4 border-y border-gray-300`}
-                  >
-                    <QuestionMarkCircleIcon
-                      color="gray"
-                      opacity={0.6}
-                      size={20}
-                    />
-                    <Text style={tw`pl-2 flex-1 text-md font-bold`}>
-                      Have a food allergy? Good
-                    </Text>
-                    <ChevronRightIcon color="#00CCBB" />
-                  </TouchableOpacity>
+                    <View style={tw`flex-row items-center space-x-1`}>
+                      <MapPinIcon color="gray" opacity={0.6} size={22} />
+                      <Text style={tw`text-xs text-gray-500`}>
+                        Nearby - {data.address}{" "}
+                      </Text>
+                    </View>
+                  </View>
+                  <Text style={tw`text-gray-500 mt-2 pb-4`}>
+                    {data.description}
+                  </Text>
                 </View>
-              </>
-            );
-          })}
-        </View>
+
+                <TouchableOpacity
+                  style={tw`flex-row items-center space-x-2 p-4 border-y border-gray-300`}
+                >
+                  <QuestionMarkCircleIcon
+                    color="gray"
+                    opacity={0.6}
+                    size={20}
+                  />
+                  <Text style={tw`pl-2 flex-1 text-md font-bold`}>
+                    Have a food allergy? Good
+                  </Text>
+                  <ChevronRightIcon color="#00CCBB" />
+                </TouchableOpacity>
+              </View>
+            </>
+          );
+        })}
       </ScrollView>
     </SafeAreaView>
   );
